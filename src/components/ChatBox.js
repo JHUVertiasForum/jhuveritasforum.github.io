37c3caddec7732e5362f673f3d76515874afa852
Jonathan Young <jonathany330@gmail.com>
2024-02-15 21:40:07 -0500

    spelling mistakes fixed

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -6,7 +6,7 @@ function seriesOfText() {
 
     const listOfText = [
         "There is a saying, yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.",
-        "We all have the opprounity to make the most of today. Yet the stressors and burdens of everyday life may weigh us down heavily.",
+        "We all have the opportunity to make the most of today. Yet the stressors and burdens of everyday life may weigh us down heavily.",
         "And when we they do, we are faced with the question...",
         "Why do we even try?",
         "Share your thoughts below..."
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,7 +3,7 @@ import veritas_logo from '../images/veritas_logo.svg';
 function title() {
     return (
         <div className='playfair'>
-            <p style = {{fontSize: '40px'}}>The Vertias Forum at Johns Hopkins</p>
+            <p style = {{fontSize: '40px'}}>The Veritas Forum at Johns Hopkins</p>
         </div>
     );
 }
